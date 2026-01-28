6ccda0e2fe8f35ca092c4a7708379c026d5a0e01
star1water1 <97842783+star1water1@users.noreply.github.com>
2026-01-28 19:01:49 +0900

    Create index.js

---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+/**
+ * Constants index
+ * 
+ * 모든 상수를 한 곳에서 import 할 수 있도록 re-export
+ * 
+ * 사용법:
+ * import { LightTheme, DarkTheme, PLATFORM_URLS } from './src/constants';
+ */
+
+export * from './theme';
+// export * from './options';  // Step 2에서 추가
+// export * from './tags';     // Step 3에서 추가
