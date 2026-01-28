06ba88df90b8fa3c951be5712cc7e224f4ede19d
star1water1 <97842783+star1water1@users.noreply.github.com>
2026-01-28 19:50:07 +0900

    Create index.js

---
+++ rc/utils/index.js
@@ -0,0 +1,12 @@
+/**
+ * Utils index
+ * 
+ * 모든 유틸리티를 한 곳에서 import 할 수 있도록 re-export
+ * 
+ * 사용법:
+ * import { uuid, pairKey, parsePlatforms } from './src/utils';
+ */
+
+export * from './helpers';
+// export * from './database';  // Step 5에서 추가
+// export * from './elo';       // Step 6에서 추가
