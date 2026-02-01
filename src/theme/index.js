3685c8a1fc96bc2e4fc5b8c4a663bafee9fe5f57
star1water1 <97842783+star1water1@users.noreply.github.com>
2026-02-01 10:35:30 +0900

    Create index.js

---
+++ src/theme/index.js
@@ -0,0 +1,13 @@
+/**
+ * src/theme/index.js
+ * 테마 모듈 re-export
+ * 
+ * v4.0 Step 32
+ */
+
+export { 
+  getCurrentTheme, 
+  getIsDark, 
+  setCurrentTheme, 
+  setThemeObject 
+} from './currentTheme';
